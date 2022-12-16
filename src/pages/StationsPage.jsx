9c43020a4766e3634ef31641ae2aaf7630caba68
Valera <portnovavalerija@mail.ru>
2022-12-16 12:47:15 +0200

    Add StationsPage realization

--- src/pages/StationsPage.jsx
+++ src/pages/StationsPage.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export const StationsPage = () => {
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [error, setError] = useState('');
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://localhost:7180/api/station")
+        .then(res => {
+            setItems(res.data);
+            setIsLoaded(false);
+        })
+        .catch(e => setError(e));
+    }, [])
+
+   
     return (
-        <div>
-            Stations Page
-        </div>
-    )
-}
+        <>
+            {isLoaded &&
+                <p> Loading... </p>}
+            {error ? 
+                <p>
+                    Error {error.message}
+                </p> :
+                <>
+                    <ul>
+                        {items.map(item => (
+                            <li key = {item.Name}>
+                                {item.name}, {item.address} 
+                            </li>
+                        ))}
+                    </ul>
+                </>                                
+            }
+        </>
+    );
+};
